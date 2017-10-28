2ca09c213c144514615572768061ae0dca21268b
Jungmin Ji <fdev.jm@gmail.com>
2017-10-28 15:22:31 +0900

    render a text to HTML

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
+/* eslint-disable */
+import React from 'react';
+import ReactDOM from 'react-dom';
 // Create a new component.
 // This component should produce some HTML
 
-/* eslint-disable */
-const App = function () {
-  return <div>Hi!</div>;
+// ES5
+// const App = function () {
+//   return <div>Learning React!</div>;
+// }
+
+// ES6
+const App = () => {
+  return <div>Learning React!</div>;
 }
 
 // Take this component's generated HTML and put it on the page
 // (in the DOM)
+const container = document.querySelector('.container')
+ReactDOM.render(<App />, container);
