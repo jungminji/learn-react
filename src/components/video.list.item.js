c16803443a9cebf9b739bbae62bbbab3121be162
Jungmin Ji <fdev.jm@gmail.com>
2017-10-29 21:27:56 +0900

    ES6 {video} access property from the first object param

--- src/components/video.list.item.js
+++ src/components/video.list.item.js
@@ -1,8 +1,23 @@
 /* eslint-disable */
 import React from 'react';
 
-const VideoListItem = (props) => {
-  return <li>Video</li>
+const VideoListItem = ({video}) => {
+  // const video = props.video
+  // {video} is identical to const video ...
+  const imgSrc = video.snippet.thumbnails.default.url;
+
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imgSrc}/>
+        </div>
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoListItem;
